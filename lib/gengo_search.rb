31300a09f790f25db857cf06544cab8970f1aff1
湯浅大樹 <yuasadaiki@yuasadaikinoMacBook-puro.local>
2019-01-16 00:17:06 +0900

    create logic

--- lib/gengo_search.rb
+++ lib/gengo_search.rb
@@ -1,6 +1,13 @@
 require "gengo_search/version"
+require 'csv'
 
 module GengoSearch
-  class Error < StandardError; end
-  # Your code goes here...
+  ERAS = CSV.read('lib/data/era_names.csv')
+
+  def self.gengo
+    ERAS.each do |en|
+      puts en[0]
+    end
+  end
+
 end
