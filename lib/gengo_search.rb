f6aa8e94a662216853cbd53491ccf74dd3bc5bf9
湯浅大樹 <yuasadaiki@yuasadaikinoMacBook-puro.local>
2019-01-17 00:00:06 +0900

    update module

--- lib/gengo_search.rb
+++ lib/gengo_search.rb
@@ -2,14 +2,20 @@ require "gengo_search/version"
 require 'csv'
 
 module GengoSearch
+  attr_reader :kanji, :romaji
   ERAS = CSV.read('lib/data/era_names.csv')
 
-  def get(year = nil)
-    if year > 0 || year <= 9999
-      puts "number"
+  def self.gengo(year = nil, type = "kanji")
+    if year.nil? then
+      puts "please set a correct number(645~9999)"
+    elsif year < 645 || year > 9999 then
+      puts "please set a correct number(645~9999)"
     else
-      @kanji = "正しい西暦を入力してください"
-      @romaji = "please set number"
+      if type == "kanji" then
+        puts "漢字"
+      else
+        puts year.to_s
+      end
     end
     #ERAS.each do |en|
     #  puts en["started_year"]
